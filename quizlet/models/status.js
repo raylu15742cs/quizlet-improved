55d56323f050383d37c674fae62978e0bbd95e7e
raylu15742cs <raylucs15742@gmail.com>
2022-12-05 11:30:25 -0800

    add title and status

---
+++ quizlet/models/status.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const StatusSchema = new Schema({
+  title: { type: Schema.Types.ObjectId, ref: 'Title', required: true }, // reference to the associated book
+  status: {
+    type: String,
+    required: true,
+    enum: ['Beginner', 'Intermediate', 'Advance', 'Master'],
+    default: 'Beginner',
+  },
+});
+
+// Virtual for bookinstance's URL
+StatusSchema.virtual('url').get(function () {
+  // We don't use an arrow function as we'll need the this object
+  return `/catalog/status/${this._id}`;
+});
+
+// Export model
+module.exports = mongoose.model('Status', StatusSchema);
---
+++ quizlet/models/title.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const TitleSchema = new Schema({
+  title: { type: String, required: true },
+  definition: { type: Schema.Types.ObjectId, ref: 'Definition', required: true },
+  status: [{ type: Schema.Types.ObjectId, ref: 'Status' }],
+});
+
+// Virtual for book's URL
+BookSchema.virtual('url').get(function () {
+  // We don't use an arrow function as we'll need the this object
+  return `/catalog/title/${this._id}`;
+});
+
+// Export model
+module.exports = mongoose.model('Title', TitleSchema);
