ea6a82a3aa5678aea45f70c1e3dbae7a8839c5da
raylu15742cs <raylucs15742@gmail.com>
2022-12-05 14:26:06 -0800

    update populatedb

--- quizlet/populatedb.js
+++ quizlet/populatedb.js
@@ -19,6 +19,7 @@ var Status = require("./models/status")
 var Title = require("./models/title")
 
 var mongoose = require('mongoose');
+const status = require('./models/status');
 var mongoDB = userArgs[0];
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = global.Promise;
@@ -210,110 +211,16 @@ function createStatuses(cb) {
   );
 }
 
-function createBookInstances(cb) {
-  async.parallel(
-    [
-      function (callback) {
-        bookInstanceCreate(
-          books[0],
-          'London Gollancz, 2014.',
-          false,
-          'Available',
-          callback
-        );
-      },
-      function (callback) {
-        bookInstanceCreate(
-          books[1],
-          ' Gollancz, 2011.',
-          false,
-          'Loaned',
-          callback
-        );
-      },
-      function (callback) {
-        bookInstanceCreate(
-          books[2],
-          ' Gollancz, 2015.',
-          false,
-          false,
-          callback
-        );
-      },
-      function (callback) {
-        bookInstanceCreate(
-          books[3],
-          'New York Tom Doherty Associates, 2016.',
-          false,
-          'Available',
-          callback
-        );
-      },
-      function (callback) {
-        bookInstanceCreate(
-          books[3],
-          'New York Tom Doherty Associates, 2016.',
-          false,
-          'Available',
-          callback
-        );
-      },
-      function (callback) {
-        bookInstanceCreate(
-          books[3],
-          'New York Tom Doherty Associates, 2016.',
-          false,
-          'Available',
-          callback
-        );
-      },
-      function (callback) {
-        bookInstanceCreate(
-          books[4],
-          'New York, NY Tom Doherty Associates, LLC, 2015.',
-          false,
-          'Available',
-          callback
-        );
-      },
-      function (callback) {
-        bookInstanceCreate(
-          books[4],
-          'New York, NY Tom Doherty Associates, LLC, 2015.',
-          false,
-          'Maintenance',
-          callback
-        );
-      },
-      function (callback) {
-        bookInstanceCreate(
-          books[4],
-          'New York, NY Tom Doherty Associates, LLC, 2015.',
-          false,
-          'Loaned',
-          callback
-        );
-      },
-      function (callback) {
-        bookInstanceCreate(books[0], 'Imprint XXX2', false, false, callback);
-      },
-      function (callback) {
-        bookInstanceCreate(books[1], 'Imprint XXX3', false, false, callback);
-      },
-    ],
-    // Optional callback
-    cb
-  );
-}
+
 
 async.series(
-  [createGenreAuthors, createBooks, createBookInstances],
+  [createCollection, createDefinition, createStatuses, createTitles],
   // Optional callback
   function (err, results) {
     if (err) {
       console.log('FINAL ERR: ' + err);
     } else {
-      console.log('BOOKInstances: ' + bookinstances);
+      console.log('Status: ' + status);
     }
     // All done, disconnect from database
     mongoose.connection.close();
