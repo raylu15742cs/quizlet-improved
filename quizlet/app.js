aae267fc0b5b776693baac14ecf4fec3a60298de
raylu15742cs <raylucs15742@gmail.com>
2022-12-09 11:57:12 -0800

    prep for railway

--- quizlet/app.js
+++ quizlet/app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const catalogRouter = require("./routes/catalog"); //Import routes for "catalog" area of site
+const compression = require('compression');
+const helmet = require('helmet');
 
 var app = express();
 
@@ -20,6 +22,7 @@ db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 
 // view engine setup
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
@@ -27,6 +30,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(compression()); // Compress all routes
+app.use(helmet());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
